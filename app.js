8790133e7c21bd49d03d5767fb6d7f7be2d34279
AchiGel <ar.gelashvili@gmail.com>
2024-01-12 01:07:58 +0400

    optimize code

--- app.js
+++ app.js
@@ -2,30 +2,40 @@ const api =
   "https://raw.githubusercontent.com/AchiGel/time-tracking-dashboard-main/main/data.json";
 
 const buttons = document.querySelectorAll(".intensiv button");
-const workCurr = document.querySelector("#work-current");
-const workPrev = document.querySelector("#work-previous");
-const playCurr = document.querySelector("#play-current");
-const playPrev = document.querySelector("#play-previous");
-const studyCurr = document.querySelector("#study-current");
-const studyPrev = document.querySelector("#study-previous");
-const exerciseCurr = document.querySelector("#Exercise-current");
-const exercisePrev = document.querySelector("#Exercise-previous");
-const socialCurr = document.querySelector("#Social-current");
-const socialPrev = document.querySelector("#Social-previous");
-const selfCareCurr = document.querySelector("#Self-Care-current");
-const selfCarePrev = document.querySelector("#Self-Care-previous");
 
 const fetchedData = [];
 
 async function getData() {
-  const response = await fetch(api);
-  const data = await response.json();
-  data.forEach((element) => {
-    fetchedData.push(element);
-  });
+  try {
+    const response = await fetch(api);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error();
+    }
+    data.forEach((element) => {
+      fetchedData.push(element);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getData();
+
+function updateUI(activity, time) {
+  fetchedData.forEach((element) => {
+    const title = element.title;
+    const current = element.timeframes[`${time}`].current;
+    const previous = element.timeframes[`${time}`].previous;
+    const activityCurrent = document.querySelector(`#${activity}-current`);
+    const activityPrevious = document.querySelector(`#${activity}-previous`);
+
+    if (title.toLowerCase().replace(" ", "-") === `${activity}`) {
+      activityCurrent.innerText = current;
+      activityPrevious.innerText = previous;
+    }
+  });
+}
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -33,87 +43,11 @@ buttons.forEach((btn) => {
 
     const target = e.target.textContent;
 
-    if (target === "Daily") {
-      fetchedData.forEach((el) => {
-        if (el.title === "Work") {
-          workCurr.textContent = el.timeframes.daily.current;
-          workPrev.textContent = el.timeframes.daily.previous;
-        }
-        if (el.title === "Play") {
-          playCurr.textContent = el.timeframes.daily.current;
-          playPrev.textContent = el.timeframes.daily.previous;
-        }
-        if (el.title === "Study") {
-          studyCurr.textContent = el.timeframes.daily.current;
-          studyPrev.textContent = el.timeframes.daily.previous;
-        }
-        if (el.title === "Exercise") {
-          exerciseCurr.textContent = el.timeframes.daily.current;
-          exercisePrev.textContent = el.timeframes.daily.previous;
-        }
-        if (el.title === "Social") {
-          socialCurr.textContent = el.timeframes.daily.current;
-          socialPrev.textContent = el.timeframes.daily.previous;
-        }
-        if (el.title === "Self Care") {
-          selfCareCurr.textContent = el.timeframes.daily.current;
-          selfCarePrev.textContent = el.timeframes.daily.previous;
-        }
-      });
-    } else if (target === "Weekly") {
-      fetchedData.forEach((el) => {
-        if (el.title === "Work") {
-          workCurr.textContent = el.timeframes.weekly.current;
-          workPrev.textContent = el.timeframes.weekly.previous;
-        }
-        if (el.title === "Play") {
-          playCurr.textContent = el.timeframes.weekly.current;
-          playPrev.textContent = el.timeframes.weekly.previous;
-        }
-        if (el.title === "Study") {
-          studyCurr.textContent = el.timeframes.weekly.current;
-          studyPrev.textContent = el.timeframes.weekly.previous;
-        }
-        if (el.title === "Exercise") {
-          exerciseCurr.textContent = el.timeframes.weekly.current;
-          exercisePrev.textContent = el.timeframes.weekly.previous;
-        }
-        if (el.title === "Social") {
-          socialCurr.textContent = el.timeframes.weekly.current;
-          socialPrev.textContent = el.timeframes.weekly.previous;
-        }
-        if (el.title === "Self Care") {
-          selfCareCurr.textContent = el.timeframes.weekly.current;
-          selfCarePrev.textContent = el.timeframes.weekly.previous;
-        }
-      });
-    } else {
-      fetchedData.forEach((el) => {
-        if (el.title === "Work") {
-          workCurr.textContent = el.timeframes.monthly.current;
-          workPrev.textContent = el.timeframes.monthly.previous;
-        }
-        if (el.title === "Play") {
-          playCurr.textContent = el.timeframes.monthly.current;
-          playPrev.textContent = el.timeframes.monthly.previous;
-        }
-        if (el.title === "Study") {
-          studyCurr.textContent = el.timeframes.monthly.current;
-          studyPrev.textContent = el.timeframes.monthly.previous;
-        }
-        if (el.title === "Exercise") {
-          exerciseCurr.textContent = el.timeframes.monthly.current;
-          exercisePrev.textContent = el.timeframes.monthly.previous;
-        }
-        if (el.title === "Social") {
-          socialCurr.textContent = el.timeframes.monthly.current;
-          socialPrev.textContent = el.timeframes.monthly.previous;
-        }
-        if (el.title === "Self Care") {
-          selfCareCurr.textContent = el.timeframes.monthly.current;
-          selfCarePrev.textContent = el.timeframes.monthly.previous;
-        }
-      });
-    }
+    updateUI("work", target.toLowerCase());
+    updateUI("play", target.toLowerCase());
+    updateUI("study", target.toLowerCase());
+    updateUI("exercise", target.toLowerCase());
+    updateUI("social", target.toLowerCase());
+    updateUI("self-care", target.toLowerCase());
   });
 });
